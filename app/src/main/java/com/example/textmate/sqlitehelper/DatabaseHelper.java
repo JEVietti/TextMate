96add1234baf1df1929e684c965b637ed3322b40
mediix <nazar.medi@gmail.com>
2015-11-16 01:05:07 -0800

    Database design/creation is final according to project requirement.

--- app/src/main/java/com/example/textmate/sqlitehelper/DatabaseHelper.java
+++ app/src/main/java/com/example/textmate/sqlitehelper/DatabaseHelper.java
@@ -2,20 +2,27 @@
 // which we need to store values from the SMSDataBase we need for the application/algorithm
 package com.example.textmate.sqlitehelper;
 
-
 import java.lang.String;
+import java.text.SimpleDateFormat;
+import java.sql.Date;
+import java.util.Locale;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 // Create a helper object to create, open, and/or manage a database.
 public class DatabaseHelper extends SQLiteOpenHelper {
+    //
+    SQLiteDatabase db;
 
     // Database name and Version for in-app use
     public static final String DATABASE_NAME = "TextMate.db";
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 16;
 
     // Database table name
     /* Threads are continuous conversations with a foreign recipient
@@ -25,9 +32,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String THREAD_TABLE = "threads";
     public static final String SMS_TABLE = "sms";
 
+    // SMS_TABLE columns
+    public static final String SMS_ID = "_id";
+    public static final String THREAD_ID_REF = "thread_id";     // Foreign key to reference THREAD_TABLE
+    public static final String ADDRESS = "address";             // Recipient address
+    public static final String PERSON = "person";               // Recipient contact name
+    public static final String DATE_RECEIVED = "date_received";
+    public static final String DATE_SENT = "date_sent";
+    public static final String BODY = "body";
+    public static final String TYPE = "type";
+    public static final String DIFF_SENT_TIME = "diff_sent_time";
+    public static final String DIFF_RETURN_TIME = "diff_return_time";
+
     // THREADS_TABLE columns
     public static final String THREAD_ID = "_id";
-    public static final String THREAD_DATE = "date";           // Created date for thread
+    public static final String THREAD_DATE = "date_initiated";           // Created date for thread
     public static final String MESSAGE_COUNT = "message_count";
     public static final String RECIPIENT = "recipient";
     public static final String SCORE_TODAY = "today_score";
@@ -38,24 +57,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TIMESTAMP = "Timestamp";
 
 
-    // SMS_TABLE columns
-    public static final String SMS_ID = "_id";
-    public static final String THREAD_ID_REF = "thread_id";      // Foreign key to reference THREAD_TABLE
-    public static final String ADDRESS = "address";        // Recipient address
-    public static final String PERSON = "person";         // Recipient contact name
-    public static final String RECEIVED_DATE = "received_date";
-    public static final String SENT_DATE = "sent_date";
-    public static final String BODY = "body";
-    public static final String TYPE = "type";
-    public static final String DIFF_SENT_TIME = "diff_sent_time";
-    public static final String DIFF_RETURN_TIME = "diff_return_time";
+    public static final String CREATE_SMS_TABLE = "CREATE TABLE " + SMS_TABLE + "("
+            + SMS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + THREAD_ID_REF + " INTEGER NOT NULL, "
+            + ADDRESS + " TEXT, "
+            + PERSON + " INTEGER DEFAULT 0, "
+            + DATE_RECEIVED + " DATETIME, "
+            + DATE_SENT + " DATETIME, "
+            + BODY + " TEXT, "
+            + TYPE + " INTEGER, "
+            + DIFF_SENT_TIME + " INTEGER DEFAULT 0, "
+            + DIFF_RETURN_TIME + " INTEGER DEFAULT 0, "
+            + BIRTH + " DATETIME, "
+            + TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP, "
+            + "UNIQUE(" + DATE_RECEIVED + ") ON CONFLICT IGNORE, "
+            + "FOREIGN KEY(" + THREAD_ID_REF + ") REFERENCES " + THREAD_TABLE + "(_id));";
 
 
     public static final String CREATE_THREADS_TABLE = "CREATE TABLE " + THREAD_TABLE + "("
-            + THREAD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + THREAD_DATE + " INTEGER DEFAULT 0, "
+            + THREAD_ID + " INTEGER PRIMARY KEY, "
+            + THREAD_DATE + " DATETIME, "
             + MESSAGE_COUNT + " INTEGER DEFAULT 0, "
-            + RECIPIENT + " TEXT, "
+            + RECIPIENT + " INTEGER DEFAULT 0, "
             + SCORE_TODAY + " REAL, "
             + SCORE_YESTERDAY + " REAL, "
             + SCORE_AVERAGE + " REAL, "
@@ -63,25 +86,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             + BIRTH + " DATETIME, "
             + TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP" + ");";
 
-    public static final String CREATE_SMS_TABLE = "CREATE TABLE " + SMS_TABLE + "("
-            + SMS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + ADDRESS + " TEXT, "
-            + PERSON + " INTEGER, "
-            + RECEIVED_DATE + " INTEGER, "
-            + SENT_DATE + " INTEGER, "
-            + BODY + " TEXT, "
-            + TYPE + " TEXT, "
-            + DIFF_SENT_TIME + " INTEGER DEFAULT 0, "
-            + DIFF_RETURN_TIME + " INTEGER DEFAULT 0, "
-            + BIRTH + " DATETIME, "
-            + TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP, "
-            + THREAD_ID_REF + " INTEGER, "
-            + "FOREIGN KEY(" + THREAD_ID_REF + ") REFERENCES " + THREAD_TABLE + "(_id));";
-
     // Class Constructor
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        SQLiteDatabase db = this.getWritableDatabase();
+        db = this.getWritableDatabase();
     }
 
     // onOpen method to enable the foreign key constraint
@@ -97,8 +105,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // onCreate Method is basically the Constructor of the DB Class
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_THREADS_TABLE);
         db.execSQL(CREATE_SMS_TABLE);
+        db.execSQL(CREATE_THREADS_TABLE);
     }
 
     // onUpgrade Method is updating the Database Version when Creating a new Version of an existing DB
@@ -109,47 +117,98 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    // Inserts the Actual Data into the DB, boolean instead of void to ensure that data is inserted correctly
-    // and the db.insert method returns -1 if it is not so we check to make sure it is not to ensure that
-    // the data is inserted correctly
-    public long insert_thread(int date, int msgCount, String recipIds) {
-        //
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues val = new ContentValues();
-
-        val.put(THREAD_DATE, date);        //The Name of the Contacts
-        val.put(MESSAGE_COUNT, msgCount);  //Initial Character Count per Day
-        val.put(RECIPIENT, recipIds);      //Initial Message Count per Day
-        val.put(BIRTH, "(NOW)");
-
-        return db.insert(THREAD_TABLE, null, val);
+    // Converts from UNIX datetime format into SQLite readable format
+    private String getDateTime(long data) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        Date date = new Date(data);
+        return dateFormat.format(date);
     }
 
     // Inserts the sms data from the built-in Android database.
-    public boolean insert_sms(int t_id, String address, int person,
-                              int recvDate, int sentDate, String body, String type) {
+    public void insert_sms(long t_id, String address, int person,
+                              long date, String body, int type, String flag) {
         //
-        SQLiteDatabase db = this.getWritableDatabase();
+        db = this.getWritableDatabase();
         ContentValues val = new ContentValues();
 
         val.put(THREAD_ID_REF, t_id);
         val.put(ADDRESS, address);
         val.put(PERSON, person);
-        val.put(RECEIVED_DATE, recvDate);
-        val.put(SENT_DATE, sentDate);
+        if (flag.equals("INCOMING")) {
+            val.put(DATE_RECEIVED, getDateTime(date));
+        }
+        else
+            val.put(DATE_SENT, getDateTime(date));
         val.put(BODY, body);
         val.put(TYPE, type);
-        val.put(BIRTH, "(NOW)");
+        val.put(BIRTH, getDateTime(java.lang.System.currentTimeMillis()));
+        //
+        try {
+            db.insertOrThrow(SMS_TABLE, null, val);
+        } catch (SQLException e) {
+            Log.d("insert-sms ->", "INSERTION Failed!");
+        }
+    }
 
-        long check = db.insert(SMS_TABLE, null, val);
+    // populateThread will read SMS data from the SMS_table and
+    // auto-populate the thread table base on schema design.
+    public void populateThread() {
+        //
+        db = this.getWritableDatabase();
 
-        if (check != -1)
-            return true;
-        else
-            return false;
+        String FETCH_THREAD_ID = "SELECT thread_id FROM sms GROUP BY thread_id;";
+        Cursor cursor1 = db.rawQuery(FETCH_THREAD_ID, null);
+        try {
+            if (cursor1.moveToFirst()) {
+                do {
+                    // Query to get the data related to specific thread_id
+                    String FETCH_THREAD_DATA = String.format(
+                            "SELECT thread_id, count(thread_id) AS cnt, MIN(date_received) AS firstMsg FROM sms WHERE thread_id=%s;",
+                                cursor1.getLong(cursor1.getColumnIndex("thread_id")));
+                    Cursor cursor2 = db.rawQuery(FETCH_THREAD_DATA, null);
+                    if (cursor2 != null) {
+                        // retrieve the data related to a thread_id
+                        cursor2.moveToFirst();
+                        long thread_id = cursor2.getLong(cursor2.getColumnIndex("thread_id"));
+                        int msgCount = cursor2.getInt(cursor2.getColumnIndex("cnt"));
+                        String lastSent = cursor2.getString(cursor2.getColumnIndex("firstMsg"));
+                        // Insert into THREAD table
+                        try {
+                            insert_thread(thread_id, msgCount, lastSent);
+                        } catch (SQLException e) {
+                            Log.d("in-dbHelper(pThread) ->", "INSERTION Failed!");
+                        }
+                        cursor2.close();
+                    }
+                } while (cursor1.moveToNext());
+                cursor1.close();
+            }
+        } catch (SQLException e) {
+            Log.d("dbHelper(pThread) -> ", "INSERTION Failed!");
+        }
+    }
+
+    // Inserts the Actual Data into the DB, boolean instead of void to ensure that data is inserted correctly
+    // and the db.insert method returns -1 if it is not so we check to make sure it is not to ensure that
+    // the data is inserted correctly
+    public void insert_thread(long tID, int msgCount, String t_date) {
+        //
+        db = this.getWritableDatabase();
+        ContentValues val = new ContentValues();
+
+        val.put(THREAD_ID, tID);
+        val.put(MESSAGE_COUNT, msgCount);  // Number of the messages in the thread
+        val.put(THREAD_DATE, t_date);
+        val.put(BIRTH, getDateTime(java.lang.System.currentTimeMillis()));
+
+        try {
+            db.insertOrThrow(THREAD_TABLE, null, val);
+        } catch (SQLException e) {
+            Log.d("insertThread ->", "INSERTION Failed!");
+        }
     }
 }
-/*
+    /*
     //////////////////////////////TextMateData Table/////////////////////////////////////////////
 
     //Inserts the Actual Data into the DB, boolean instead of void to ensure that data is inserted correctly
