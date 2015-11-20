f37c04d91fb296ced731769488d6d81f75fdeb4f
mediix <nazari.medi@gmail.com>
2015-11-19 20:30:43 -0800

    Added word_count and total_word_count in the appropriate tables

--- app/src/main/java/com/example/textmate/sqlitehelper/DatabaseHelper.java
+++ app/src/main/java/com/example/textmate/sqlitehelper/DatabaseHelper.java
@@ -25,7 +25,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     // Database name and Version for in-app use
     public static final String DATABASE_NAME = "TextMate.db";
-    public static final int DATABASE_VERSION = 16;
+    public static final int DATABASE_VERSION = 32;
 
     // Database table name
     /* Threads are continuous conversations with a foreign recipient
@@ -43,15 +43,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATE_RECEIVED = "date_received";
     public static final String DATE_SENT = "date_sent";
     public static final String BODY = "body";
+    public static final String WORD_COUNT = "word_count";
     public static final String TYPE = "type";
-    public static final String DIFF_SENT_TIME = "diff_sent_time";
-    public static final String DIFF_RETURN_TIME = "diff_return_time";
 
     // THREADS_TABLE columns
     public static final String THREAD_ID = "_id";
     public static final String THREAD_DATE = "date_initiated";           // Created date for thread
     public static final String MESSAGE_COUNT = "message_count";
     public static final String RECIPIENT = "recipient";
+    public static final String TOTAL_WORD_COUNT = "total_word_count";
+    public static final String DIFF_SENT_TIME = "diff_sent_time";
+    public static final String DIFF_RETURN_TIME = "diff_return_time";
     public static final String SCORE_TODAY = "today_score";
     public static final String SCORE_YESTERDAY = "yesterday_score";
     public static final String SCORE_AVERAGE = "average_score";
@@ -68,9 +70,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             + DATE_RECEIVED + " DATETIME, "
             + DATE_SENT + " DATETIME, "
             + BODY + " TEXT, "
+            + WORD_COUNT + " INTEGER DEFAULT 0, "
             + TYPE + " INTEGER, "
-            + DIFF_SENT_TIME + " INTEGER DEFAULT 0, "
-            + DIFF_RETURN_TIME + " INTEGER DEFAULT 0, "
             + BIRTH + " DATETIME, "
             + TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP, "
             + "UNIQUE(" + DATE_RECEIVED + ") ON CONFLICT IGNORE, "
@@ -81,10 +82,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             + THREAD_ID + " INTEGER PRIMARY KEY, "
             + THREAD_DATE + " DATETIME, "
             + MESSAGE_COUNT + " INTEGER DEFAULT 0, "
-            + RECIPIENT + " INTEGER DEFAULT 0, "
-            + SCORE_TODAY + " REAL, "
-            + SCORE_YESTERDAY + " REAL, "
-            + SCORE_AVERAGE + " REAL, "
+            + RECIPIENT + " TEXT DEFAULT NULL, "
+            + TOTAL_WORD_COUNT + " INTEGER DEFAULT 0, "
+            + DIFF_SENT_TIME + " INTEGER DEFAULT 0, "
+            + DIFF_RETURN_TIME + " INTEGER DEFAULT 0, "
+            + SCORE_TODAY + " REAL DEFAULT 0, "
+            + SCORE_YESTERDAY + " REAL DEFAULT 0, "
+            + SCORE_AVERAGE + " REAL DEFAULT 0, "
             + NUM_OF_UPDATES + " INTEGER DEFAULT 0, "
             + BIRTH + " DATETIME, "
             + TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP" + ");";
@@ -127,6 +131,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return dateFormat.format(date);
     }
 
+    // Counts the number of words in the body of a SMS.
+    private int wordCount(String body) {
+        //
+        int wordCount = 0;
+
+        if (body.trim().equals("")) return wordCount;
+        else wordCount = 1;
+
+        for (int i = 0; i < body.length(); i++) {
+            char ch = body.charAt(i);
+            String str = String.format("%s", ch);
+            if (i+1 != body.length() && str.equals(" ") && !(""+ body.charAt(i+1)).equals(" ")) {
+                wordCount++;
+            }
+        }
+
+        return wordCount;
+    }
+
     // Inserts the sms data from the built-in Android database.
     public void insert_sms(long t_id, String address, int person,
                               long date, String body, int type, String flag) {
@@ -141,9 +164,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         if (flag.equals("INCOMING")) {
             val.put(DATE_RECEIVED, getDateTime(date));
         }
-        else
+        else {
             val.put(DATE_SENT, getDateTime(date));
+        }
         val.put(BODY, body);
+        val.put(WORD_COUNT, wordCount(body));
         val.put(TYPE, type);
         val.put(BIRTH, getDateTime(java.lang.System.currentTimeMillis()));
         //
@@ -167,18 +192,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 do {
                     // Query to get the data related to specific thread_id
                     String FETCH_THREAD_DATA = String.format(
-                            "SELECT thread_id, count(thread_id) AS cnt, MIN(date_received) AS firstMsg FROM sms WHERE thread_id=%s;",
+                            "SELECT thread_id, address, " +
+                                    "COUNT(thread_id) AS cnt, " +
+                                    "SUM(word_count) AS totalWordCount, " +
+                                    "MIN(date_received) AS firstMsg " +
+                                    "FROM sms WHERE thread_id=%s;",
                                 cursor1.getLong(cursor1.getColumnIndex("thread_id")));
                     Cursor cursor2 = db.rawQuery(FETCH_THREAD_DATA, null);
                     if (cursor2 != null) {
                         // retrieve the data related to a thread_id
                         cursor2.moveToFirst();
                         long thread_id = cursor2.getLong(cursor2.getColumnIndex("thread_id"));
+                        String recipient = cursor2.getString(cursor2.getColumnIndex("address"));
+                        int totalWordCount = cursor2.getInt(cursor2.getColumnIndex("totalWordCount"));
                         int msgCount = cursor2.getInt(cursor2.getColumnIndex("cnt"));
                         String lastSent = cursor2.getString(cursor2.getColumnIndex("firstMsg"));
                         // Insert into THREAD table
                         try {
-                            insert_thread(thread_id, msgCount, lastSent);
+                            insert_thread(thread_id, recipient, totalWordCount, msgCount, lastSent);
                         } catch (SQLException e) {
                             Log.d("in-dbHelper(pThread) ->", "INSERTION Failed!");
                         }
@@ -195,12 +226,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // Inserts the Actual Data into the DB, boolean instead of void to ensure that data is inserted correctly
     // and the db.insert method returns -1 if it is not so we check to make sure it is not to ensure that
     // the data is inserted correctly
-    public void insert_thread(long tID, int msgCount, String t_date) {
+    public void insert_thread(long t_ID, String recipient, int totalWord, int msgCount, String t_date) {
         //
         db = this.getWritableDatabase();
         ContentValues val = new ContentValues();
 
-        val.put(THREAD_ID, tID);
+        val.put(THREAD_ID, t_ID);
+        val.put(RECIPIENT, recipient);
+        val.put(TOTAL_WORD_COUNT, totalWord);
         val.put(MESSAGE_COUNT, msgCount);  // Number of the messages in the thread
         val.put(THREAD_DATE, t_date);
         val.put(BIRTH, getDateTime(java.lang.System.currentTimeMillis()));
@@ -211,7 +244,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             Log.d("insertThread ->", "INSERTION Failed!");
         }
     }
-
+    
     /*public String getContactName(Context _context, String number) {
         String name;
         if(number != null && !number.equals("")){
@@ -236,182 +269,3 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return name;
     }*/
 }
-    /*
-    //////////////////////////////TextMateData Table/////////////////////////////////////////////
-
-    //Inserts the Actual Data into the DB, boolean instead of void to ensure that data is inserted correctly
-    //and the db.insert method returns -1 if it is not so we check to make sure it is not to ensure that
-    //the data is inserted correctly
-    public boolean Data_insertData(String ID, String name, int charCount,
-                                   int messageCount, double diffSentTime, double diffReturnTime) {
-        ContentValues val = new ContentValues();
-
-        val.put(Data_Column1, ID);               //The ID of the Contacts
-        val.put(Data_Column2, name);             //The Name of the Contacts
-        val.put(Data_Column3, charCount);        //Initial Character Count per Day
-        val.put(Data_Column4, messageCount);     //Initial Message Count per Day
-        val.put(Data_Column5, diffSentTime);     //Initial Average time between Sending for each Contact for the day
-        val.put(Data_Column6, diffReturnTime);   //Initial Average time between receiving a message for each Contact for the day
-
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        long check = db.insert(Data_Table_Name, null, val);
-        db.close();
-        return check != -1;
-    }
-
-    //The Update Data will be the values passed to it
-    public boolean Data_updateData(String ID, String name, int charCount,
-                                   int messageCount, double diffSentTime, double diffReturnTime) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues val = new ContentValues();
-
-        val.put(Data_Column1, ID);               //The ID of the Contacts
-        val.put(Data_Column2, name);             //The Name of the Contacts
-        val.put(Data_Column3, charCount);        //The Average Character Count Per Message per Day
-        val.put(Data_Column4, messageCount);     //The Average Message Count for the the Day
-        val.put(Data_Column5, diffSentTime);     //Average Amount of time between Sent Messages from Received
-        val.put(Data_Column6, diffReturnTime);
-
-
-        db.update(Data_Table_Name, val, "ID = ?", new String[]{ID}); //Update based on the ID
-        return true;
-    }
-    //Fetch a single row
-    public textMateData fetchData(long dataID){
-        SQLiteDatabase db = this.getWritableDatabase();
-        String selectQuery = "SELECT * FROM" + Data_Table_Name + "WHERE"
-                +Data_Column1+ "=" + dataID;
-        Log.e(LOG, selectQuery);
-        Cursor c = db.rawQuery(selectQuery, null);
-
-        //Use the model Functions for the Table Class
-        textMateData tmData = new textMateData();
-        if(c != null && c.moveToFirst()) {
-            tmData.setId(c.getInt(c.getColumnIndex(Data_Column1)));
-            tmData.setName(c.getString(c.getColumnIndex(Data_Column2)));
-            tmData.setCharCount(c.getInt(c.getColumnIndex(Data_Column3)));
-            tmData.setMsgCount(c.getInt(c.getColumnIndex(Data_Column4)));
-            //....
-        }
-        return tmData;
-    }
-    //Fetch all rows data
-    public List<textMateData> fetchAllData(){
-        List<textMateData> allTD = new ArrayList<textMateData>();
-        String selectQuery = "SELECT * FROM" + Data_Table_Name;
-
-        Log.e(LOG, selectQuery);
-
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor c = db.rawQuery(selectQuery,null);
-
-        if(c!=null && c.moveToFirst()) {
-            do {
-                textMateData tmData = new textMateData();
-                tmData.setId(c.getInt(c.getColumnIndex(Data_Column1)));
-                tmData.setName(c.getString(c.getColumnIndex(Data_Column2)));
-                tmData.setCharCount(c.getInt(c.getColumnIndex(Data_Column3)));
-                tmData.setMsgCount(c.getInt(c.getColumnIndex(Data_Column4)));
-                //....
-                allTD.add(tmData);
-            }
-            while (c.moveToNext());
-        }
-        return allTD;
-    }
-
-    //Updating the TextMate DataTable row
-    public int updateDataTable(textMateData tmData){
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues val = new ContentValues();
-        val.put(Data_Column3, tmData.fetchCharCount());
-        val.put(Data_Column4, tmData.fetchMsgCount());
-        //...
-
-        return db.update(Data_Table_Name,val,Data_Column1 + "= ?",
-                new String[] {String.valueOf(tmData.fetchID())});
-    }
-
-
-    //Deleting TextMate DataTable
-    public void deleteTMData(long dataID){
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(Data_Table_Name, Data_Column1 + "= ?",
-                new String[]{String.valueOf(dataID)});
-    db.close();
-    }
-
-        ///////////////////////// TextMate Algorithm Scores Table ///////////////////////////////////
-
-    //Fetch a single row
-    public textMateScores fetchScore(long dataID){
-        SQLiteDatabase db = this.getWritableDatabase();
-        String selectQuery = "SELECT * FROM" + Data_Table_Name + "WHERE"
-                +Scores_Column1+ "=" + dataID;
-        Log.e(LOG, selectQuery);
-        Cursor c = db.rawQuery(selectQuery, null);
-
-        //Use the model Functions for the Table Class
-        textMateScores tmScores = new textMateScores();
-        if(c != null && c.moveToFirst()) {
-            tmScores.setID(c.getInt(c.getColumnIndex(Scores_Column1)));
-            tmScores.setName(c.getString(c.getColumnIndex(Scores_Column2)));
-            tmScores.setScore(c.getDouble(c.getColumnIndex(Scores_Column3)));
-            tmScores.setNumUpdate(c.getInt(c.getColumnIndex(Scores_Column4)));
-            //....
-        }
-        db.close();
-        return tmScores;
-    }
-    //Fetch all rows data
-    public List<textMateScores> fetchAllScores(){
-        List<textMateScores> allTS = new ArrayList<textMateScores>();
-        String selectQuery = "SELECT * FROM" + Scores_Table_Name;
-
-        Log.e(LOG,selectQuery);
-
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor c = db.rawQuery(selectQuery,null);
-
-        if(c!=null && c.moveToFirst()) {
-            do {
-                textMateScores tmScore = new textMateScores();
-                tmScore.setID(c.getInt(c.getColumnIndex(Scores_Column1)));
-                tmScore.setName(c.getString(c.getColumnIndex(Scores_Column2)));
-                tmScore.setScore(c.getDouble(c.getColumnIndex(Scores_Column3)));
-                tmScore.setNumUpdate(c.getInt(c.getColumnIndex(Scores_Column4)));
-                //....
-                allTS.add(tmScore);
-            }
-            while (c.moveToNext());
-        }
-        db.close();
-        return allTS;
-    }
-
-    //Updating the TextMate ScoresTable row
-    public int updateScoreTable(textMateScores tmScores){
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues val = new ContentValues();
-        val.put(Scores_Column3, tmScores.fetchScore());
-        val.put(Scores_Column4,tmScores.fetchNumUpdate());
-        //...
-
-        return db.update(Scores_Table_Name,val,Scores_Column1 + "= ?",
-                new String[] {String.valueOf(tmScores.fetchID())});
-    }
-
-
-    //Deleting TextMate DataTable
-    public void deleteTMScore(long scoreID){
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(Scores_Table_Name, Scores_Column1 + "= ?",
-                new String[] {String.valueOf(scoreID)});
-        db.close();
-    }
-
-}
-*/
