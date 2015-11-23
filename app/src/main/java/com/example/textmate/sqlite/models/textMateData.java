0d49f7193845974a61e0f873a2be8689505883e4
JEVietti <dovajoe@gmail.com>
2015-11-22 16:43:40 -0800

    Changes to the textMateData class to fit with the schema and Constructor Changes to make it easier to just call from the object for data

--- app/src/main/java/com/example/textmate/sqlite/models/textMateData.java
+++ app/src/main/java/com/example/textmate/sqlite/models/textMateData.java
@@ -10,67 +10,69 @@ import com.example.textmate.sqlitehelper.DatabaseHelper;
 
 public class textMateData {
     //Class Variables
-    private int id, numUpdate, charCount, msgCount;
+    private int id, numUpdate, wordCount, msgCount;
     private String name;
     int count;
+    private double newAvgTimeSent, newAvgTimeRec, newAvgWordCount;
     private int[] diffTimeSent = new int [count];
     private int[] diffTimeReceive = new int[count];
     String createdTime; //keeps track when the database is created
     //Constructors
     public textMateData(){} //Empty Constructor
     //Constructor to initialize the data into the object of the class
-    public textMateData(int id, String name, int charCount,int msgCount,int diffSentVal[],int diffReceiveVal[], int numUpdate,int count){
+    public textMateData(int wordCount,int msgCount,int diffSentVal[],int diffReceiveVal[],int numUpdate,int count){
         this.count = count;
-        this.id = id;
-        this.name = name;
-        this.charCount = charCount;
+        //this.id = id;
+        //this.name = name;
+        this.wordCount = wordCount;
         this.msgCount = msgCount;
         for(int i=0;i<this.count;i++){
         this.diffTimeSent[i] = diffSentVal[i];
         this.diffTimeReceive[i] = diffReceiveVal[i];
         }
+        this.newAvgTimeRec=this.setDiffTimeReceive();
+        this.newAvgTimeSent=this.setDiffTimeSent();
+        this.newAvgWordCount=this.setAvgWordCount();
         this.numUpdate = numUpdate;
     }
 
     //Class Methods for setting the Values of the Columns for the textMateData Table
 
     // Get the id of a row
-    public int fetchID(){return this.id;}
+    //public int fetchID(){return this.id;}
 
     // Set a new ID
-    public void setId(int newID){this.id=newID;}
+    //public void setId(int newID){this.id=newID;}
 
     // Get the Name of row
-    public String fetchName(){return this.name;}
+   // public String fetchName(){return this.name;}
 
     // Set a new Name for a row
-    public void setName(String newName){this.name=newName;}
+    //public void setName(String newName){this.name=newName;}
 
-    //
+    //Returns the Number of Updates which may be useful because of incrementing
     public int fetchNumUpdate(){return this.numUpdate;}
 
-    //
-    public void setNumUpdate(){
+    //Increment the number of updates so the Averages for diff time receive and sent can be calculated
+    public void incNumUpdate(){
         int ans;
         ans = fetchNumUpdate()+1;
         this.numUpdate=ans;}
 
-    // Get the Character count of a row
-    public int fetchCharCount(){return this.charCount;}
+    //Gives access to AvgWordCount per Message fpr each Contact to put back into Database
+    public double fetchAvgWordCount(){return this.newAvgWordCount;}
 
-    // Set the New Character Count
-    public void setCharCount(int newCharCount){this.charCount = newCharCount;}
+    //Calculates the Average amount of words per message for each contact in the SMS table
+    public double setAvgWordCount(){
+        double ans;
+        ans = this.wordCount/this.msgCount;
+        return ans;
+    }
 
-    // get the current Message Count
-    public int fetchMsgCount(){return this.msgCount;}
+    //Gives access to AvgTimeSent to put back into Database
+    public double fetchDiffTimeRSent(){return this.newAvgTimeSent;}
 
-    // Set the new Message Count
-    public void setMsgCount(int newMsgCount){this.msgCount = newMsgCount;}
-
-    //
-    public int[] fetchDiffTimeSent(){return this.diffTimeSent;}
-
-    //
+    //Calculates and Sets the Average Difference in time between Sent Messages of a Single Contact
     public double setDiffTimeSent() {
         double temp=0.0,ans = 0.0;
         for (int i = 0; i < (this.count); i++) { ans+=temp;
@@ -81,10 +83,10 @@ public class textMateData {
         ans = ans / this.count;
         return ans;
     }
-    //
-    public int[] fetchDiffTimeReceive(){return this.diffTimeReceive;}
+    //Gives access to the AvgTimeReceived to put it back into a database
+    public double fetchDiffTimeReceive(){return this.newAvgTimeRec;}
 
-    //
+    //Calculates and sets the Average Difference in time bewtween Received Messages of a Single Contact
     public double setDiffTimeReceive(){
         double temp=0.0,ans = 0.0;
         for (int i = 0; i < (this.count); i++) { ans+=temp;
